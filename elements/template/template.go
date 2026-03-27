ddc50b1899a407f904b5a3448b5a8382c0ced715
ohzqq <iamchurkey@gmail.com>
2026-03-27 13:54:18 -0400

    add template

---
+++ elements/template/template.go
@@ -0,0 +1,39 @@
+package template
+
+import (
+	"syscall/js"
+
+	"github.com/ohzqq/tinydom"
+)
+
+type Template struct {
+	*tinydom.Element
+}
+
+func New() *Template {
+	tmpl := tinydom.GetDocument().CreateElement("template")
+	return &Template{
+		Element: tmpl,
+	}
+}
+
+func GetById(id string) *Template {
+	return WrapTemplate{tinydom.GetDocument().GetElementById(id).Underlying()}
+}
+
+func QuerySelector(sel string) *Template {
+	return WrapTemplate{tinydom.GetDocument().QuerySelector(sel).Underlying()}
+}
+
+func WrapTemplate(val js.Value) *Template {
+	return &Template{Element: tinydom.WrapElement(val)}
+}
+
+func (t *Template) ImportNode() *tinydom.DocumentFragment {
+	content := tinydom.GetDocument().Call("importNode", t.Content().Underlying(), true)
+	return tinydom.WrapDocumentFragment(content)
+}
+
+func (t *Template) Content() *tinydom.DocumentFragement {
+	return tinydom.WrapDocumentFragment(t.Underlying().Get("content"))
+}
